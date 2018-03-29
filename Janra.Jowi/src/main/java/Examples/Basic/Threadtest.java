e3db694fb39224300e1a605d84b7b63f8603bc11
Pressman <Annette.Pressman@thomsonreuters.com>
2018-03-29 16:26:31 +0100

    "instantiated TestTimer..." Signed-off-by: Pressman <Annette.Pressman@thomsonreuters.com>

--- Janra.Jowi/src/main/java/Examples/Basic/Threadtest.java
+++ Janra.Jowi/src/main/java/Examples/Basic/Threadtest.java
@@ -9,12 +9,12 @@ import Utilities.ThreadLauncher;
 
 public class Threadtest implements IPipelineMiddleware {
 	
-	private TestTimer testTimer; 
-	private Boolean trigger = false;
+	private TestTimer testTimer = new TestTimer(); 
+
 	@Override
 	public Boolean Invoke(IContext context) {
 		
-/*		Boolean started = testTimer.getStarted();
+		Boolean started = testTimer.getStarted();
 		Boolean finished = testTimer.getFinished();
 		
 		if (context.Request().method() == HttpMethod.GET) {
@@ -23,21 +23,25 @@ public class Threadtest implements IPipelineMiddleware {
 			
 		
 		
-			if ((started == false) && (finished ==false)) {
+			if (!started && !finished) {
 				context.setResponseStatus(503);
 				threadlauncher.launch(testTimer);
+				return true;
 			} else if ((started == true) && (finished ==false)) {
 				context.setResponseStatus(503);
 				context.setResponseBody("Not working yet");
+				return true;
 			} else if ((started == true) && (finished ==true)) {
 				context.setResponseStatus(200);
 				context.addResponseHeader("Content-type", "text/plain");
 				context.setResponseBody("\"instance\": \"workingProperley\"");
 				testTimer.setFinished(false);
 				testTimer.setStarted(false);
+				return true;
 			} else {
 				context.setResponseStatus(500);
 				context.setResponseBody("Started = " + started + ", finished = " + finished );
+				return true;
 			}
 		}		
 		else {
@@ -45,38 +49,9 @@ public class Threadtest implements IPipelineMiddleware {
 			context.setResponseBody("Wrong method " + context.Request().method().toString()+ ", Started = \" + started + \", finished = \" + finished");
 		}	
 		return true; 
-	}*/
+	}
 
-	trigger = isFailureCode();
-	
-	if ((context.Request().method() == HttpMethod.GET)&& (trigger == false)) { 
-		context.setResponseStatus(200);
-		context.addResponseHeader("Content-type", "text/plain");
-		context.setResponseBody("\"instance\": \"workingProperley\"");
-	}
-	else if ((context.Request().method() == HttpMethod.GET)&& (trigger == true))  {
-		context.setResponseStatus(503);
-		context.setResponseBody("Not working yet");
-		
-	}
-	else if (context.Request().method() != HttpMethod.GET) {
-		context.setResponseStatus(404);
-		context.setResponseBody("Wrong method");
-	} else {
-		context.setResponseStatus(500);
-	}
-	return false;
-}
 
-private Boolean isFailureCode() {
-	long time = System.currentTimeMillis();
-	long rem = time % 120000;
-	if (rem >= 70000) {
-		return true;
-	} else {
-		return false;
-	}
-}
 	
 }
 
