e16d868ad62d9be2db290c583fe84cbe7e53d55c
Pressman <Annette.Pressman@thomsonreuters.com>
2018-04-03 11:38:31 +0100

    test sns topic added to deploy pipeline script Signed-off-by: Pressman <Annette.Pressman@thomsonreuters.com>

--- Pipeline/deploy.sh
+++ Pipeline/deploy.sh
@@ -12,3 +12,5 @@ docker tag 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest 015887
 $(aws ecr get-login --no-include-email --region us-east-1)
 docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
 docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
+
+aws sns publish --region "eu-west-1" --topic-arn  "arn:aws:sns:eu-west-1:608014515287:AcunaBob" --message "This should be working..."
