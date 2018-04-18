10a862013ecd12830d1c2f4bb68190c4757d90de
Annette Pressman <apressman@annettes-mbp.ten.thomsonreuters.com>
2018-04-18 14:15:10 +0100

    new Go pipleine 6

--- Pipeline/build.sh
+++ Pipeline/build.sh
@@ -10,7 +10,4 @@ CURDIR=`pwd`
 echo " Current Directory is "
 echo ${CURDIR}
 
-docker run --rm \
-           -v "${CURDIR}/:/build" \
-           --workdir /build/Janra.Jowi \
-           maven:3.5.2-jdk-8 mvn compile
+docker run --rm -v "${CURDIR}/:/build" --workdir /build/Janra.Jowi maven:3.5.2-jdk-8 mvn compile
