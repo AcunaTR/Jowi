c42ab8518510451e7de14ece94c23b41a5d39d5b
Annette Pressman <apressman@annettes-mbp.ten.thomsonreuters.com>
2018-04-20 09:18:12 +0100

    Go pipleine testing

--- Pipeline/build.sh
+++ Pipeline/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 function buildFail {
     echo "Maven build failed"
 }
--- Pipeline/test.sh
+++ Pipeline/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 function testFail {
     echo "Maven test failed"
@@ -9,7 +10,4 @@ CURDIR=`pwd`
 
 echo "this is before it runs"
 
-docker run --rm \
-           -v "${CURDIR}/:/test" \
-           --workdir /test/Janra.Jowi \
-           maven:3.5.2-jdk-8 mvn test
+docker run --rm -v "${CURDIR}/:/test" --workdir /test/Janra.Jowi maven:3.5.2-jdk-8 mvn test
