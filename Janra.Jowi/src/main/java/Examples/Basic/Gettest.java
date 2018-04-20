b2b3c295ea939c5c15d13000bb20b3065d997664
Annette Pressman <apressman@annettes-mbp.ten.thomsonreuters.com>
2018-04-20 13:40:23 +0100

    testing complete Go pipline deploy step 7

--- Janra.Jowi/src/main/java/Examples/Basic/Gettest.java
+++ Janra.Jowi/src/main/java/Examples/Basic/Gettest.java
@@ -12,7 +12,7 @@ public class Gettest implements IPipelineMiddleware {
 		if (context.Request().method() == HttpMethod.GET) {
 			context.setResponseStatus(200);
 			context.addResponseHeader("Content-type", "text/plain");
-			context.setResponseBody("\"type\": \"ApplianceHealthStatus\"");
+			context.setResponseBody("\"type\": \"ItIsWorkingGoddammit\"");
 		}
 		else {
 			context.setResponseStatus(404);
