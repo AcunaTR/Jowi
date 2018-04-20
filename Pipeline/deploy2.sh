2d26aea95724207c6aa3609ccd0b49b293bac3f4
Annette Pressman <apressman@annettes-mbp.ten.thomsonreuters.com>
2018-04-20 13:22:52 +0100

    testing complete Go pipline deploy step 5

--- Pipeline/deploy2.sh
+++ Pipeline/deploy2.sh
@@ -10,8 +10,6 @@ VERSION=$(cat ./version)
 docker tag jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
 docker tag 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
 $(aws ecr get-authorization-token --region us-east-1 --output text --query 'authorizationData[].authorizationToken' | base64 -d | cut -d: -f2)
-#docker login -u AWS 015887481462.dkr.ecr.us-east-1.amazonaws.com
 docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
-docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
+docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$GO_PIPELINE_LABEL
 
-#aws sns publish --region "us-east-1" --topic-arn  "arn:aws:sns:us-east-1:015887481462:AcunaBob" --message "This should be working..."
