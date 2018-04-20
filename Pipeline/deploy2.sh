bcf20b07b138e49a1632b103e48dc57e9df876f7
Annette Pressman <apressman@annettes-mbp.ten.thomsonreuters.com>
2018-04-20 13:27:46 +0100

    testing complete Go pipline deploy step 6

--- Pipeline/deploy2.sh
+++ Pipeline/deploy2.sh
@@ -8,7 +8,7 @@ trap deployFail ERR
 VERSION=$(cat ./version)
 
 docker tag jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
-docker tag 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
+docker tag 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$GO_PIPELINE_LABEL
 $(aws ecr get-authorization-token --region us-east-1 --output text --query 'authorizationData[].authorizationToken' | base64 -d | cut -d: -f2)
 docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
 docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$GO_PIPELINE_LABEL
