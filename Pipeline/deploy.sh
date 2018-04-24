ccf50a328d4f541512f7c80ab602f8d26bd0d5cc
Andy Dean <andy_dean@hotmail.co.uk>
2018-04-24 11:28:38 +0100

    Refactor for sandbox Ireland deploy

--- Pipeline/deploy.sh
+++ Pipeline/deploy.sh
@@ -7,10 +7,10 @@ trap deployFail ERR
 
 VERSION=$(cat ./version)
 
-docker tag jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
-docker tag 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
-$(aws ecr get-login --no-include-email --region us-east-1)
-docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:latest
-docker push 015887481462.dkr.ecr.us-east-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
+docker tag jowi:latest 015887481462.dkr.ecr.eu-west-1.amazonaws.com/acuna-jowi:latest
+docker tag 015887481462.dkr.ecr.eu-west-1.amazonaws.com/acuna-jowi:latest 015887481462.dkr.ecr.eu-west-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
+$(aws ecr get-login --no-include-email --region eu-west-1)
+docker push 015887481462.dkr.ecr.eu-west-1.amazonaws.com/acuna-jowi:latest
+docker push 015887481462.dkr.ecr.eu-west-1.amazonaws.com/acuna-jowi:$VERSION.$BUILD_NUMBER
 
 #aws sns publish --region "us-east-1" --topic-arn  "arn:aws:sns:us-east-1:015887481462:AcunaBob" --message "This should be working..."
